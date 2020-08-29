15a940db51d4c929df99ca59dff1d575829f2d10
Jacquese Wilson <quese@Jodye.local>
2020-08-28 22:31:42 -0400

    Calculate integers

---
+++ src/funwithvariables/calculate.py
@@ -0,0 +1,10 @@
+
+def calculateint():
+  #value to the right will be assigned name to the left, this is called variables and values
+  x=5  #x is a variable that holds the value of integer 5
+  y=4  #with python you dont have to specify type
+  z=x+y
+  print(z)
+
+if __name__ == '__main__':
+    calculateint()

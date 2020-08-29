3ea5adb15b3e0c18c6a06eac7b761f9f8675888e
Jacquese Wilson <quese@Jodye.local>
2020-08-28 22:49:08 -0400

    Parsing Strings now

--- src/funwithvariables/calculate.py
+++ src/funwithvariables/calculate.py
@@ -12,6 +12,16 @@ def calculateintmaybe():
   z=x+y   # if y equaled "4" we will not be able to add because "" represents strings or words
   print(z) #it works because now 5+"4" becomes 5+4
 
+def calculatestrings():
+  x="5" #Okay earlier we touched on strings, now lets add them
+  y="4"
+  z=x+y #Guess the answer
+  print(z)
+  z=z+str(5) #Now, when adding values they MUST be of  the same type in Python
+  print(z)   #Additionally you may have notice we took the original z and added "5" to make a new z | change the value
+
+
 if __name__ == '__main__':
   #calculateint()
-  calculateintmaybe()
+  #calculateintmaybe()
+  calculatestrings() #Maybe not what you expect but you have to remember these are no longer numbers..they are characters/words
